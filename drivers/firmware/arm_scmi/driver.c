981e1f9d6b3f943e3d6e86782d266131ad0ceb9a
Antonio del Vecchio <antonio.delvecchio2@studio.unibo.it>
2023-06-28 16:42:05 +0200

    Update driver.c

--- drivers/firmware/arm_scmi/driver.c
+++ drivers/firmware/arm_scmi/driver.c
@@ -213,7 +213,7 @@ static void scmi_fetch_response(struct scmi_xfer *xfer,
 		packet = ioread32(mem->msg_payload + 4 + reg_cnt);
 		if(packet != 0){ printk("pack:%d",packet);}
 		for (byte_cnt = 0; byte_cnt < 4; byte_cnt++){
-			*(u8 *)(xfer->rx.buf + byte_cnt) = (u8)(packet >> (8*byte_cnt));
+			*(u8 *)(xfer->rx.buf + byte_cnt + reg_cnt) = (u8)(packet >> (8*byte_cnt));
 			if((*(u8 *)(xfer->rx.buf + byte_cnt + reg_cnt)) != 0){ printk("byte:%d",*(u8 *)(xfer->rx.buf + byte_cnt));}
 		}
 	}
